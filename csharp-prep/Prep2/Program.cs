801424d945ec5738309f05d7dbcea434176007ab
scott kishpaugh <scottjkishpaugh7@gmail.com>
2023-04-24 23:28:13 -0600

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,13 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        int numbers = 100;
-        
-        while (numbers != 0)
+        Console.Write("What is your final grade? ");
+        string studentResponse = Console.ReadLine();
+        int grade = int.Parse(studentResponse);
+
+        string letter = "";
+
+        if (grade >90)
         {
-            Console.Write("Enter in a number type 0 when done ");
-            
-           
+           letter = "A";
         }
-    }   
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your final grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have passed the class");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class");
+        }
+    }  
 }
