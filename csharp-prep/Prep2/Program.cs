de160bb9406e4e6cf9ad6002adafd7fcd83c4942
scott kishpaugh <scottjkishpaugh7@gmail.com>
2023-04-24 23:12:58 -0600

    finished prep 4

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        int numbers = 100;
         
-    }
+        while (numbers != 0)
+        {
+            Console.Write("Enter in a number type 0 when done ");
+            
+           
+        }
+    }   
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber != 0);
+        {
+            Console.Write("Enter in a number type 0 when finished ");
+            string Response = Console.ReadLine();
+            userNumber = int.Parse(Response);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sume is {sum}");
     }
 }
