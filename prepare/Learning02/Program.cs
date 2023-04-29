310b903f3b1fe8110deeca80b443d60135ef6d28
scott kishpaugh <scottjkishpaugh7@gmail.com>
2023-04-29 12:23:26 -0600

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,24 +1,3 @@
 using System;
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!"); 
-        Learning02.Program program = new Learning02.Program();
-        program.AnotherFunction();
-    }
-} 
-
-
-namespace Learning02
-{   class Program
-    {
-        public void AnotherFunction()
-        {
-            Console.WriteLine("Now I am Writeing another function.");
-        }
-    }   
-    
-}
