bfbb845825e2a6b73ed3118be33fbc934f733e1d
Scott Kishpaugh <scottkishpaugh@Scotts-MacBook-Air-2.local>
2023-06-18 21:03:00 -0600

    shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       List<Shape> shapes = new List<Shape>();
+
+       Square square = new Square("Yellow", 10);
+       shapes.Add(square);
+
+       Rectangle rectangle = new Rectangle("Purple", 5, 4);
+       shapes.Add(rectangle);
+
+       Circle circle = new Circle("White", 8);
+       shapes.Add(circle);
+
+       foreach(Shape i in shapes)
+       {
+            string color = i.GetColor();
+            double area = i.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+       }
     }
 }
